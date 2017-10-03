a14a4c1d211da992597fd096dd08683835a25011
briandyko <briandyko@gmail.com>
2017-10-03 14:44:20 -0400

    initial commit

---
+++ Week 2_Day2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    //Console.WriteLine(i);
+            //}
+
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //string greeting = "My name is Little Bill";
+            //string[] words = greeting.Split();
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+            //string[] days = { "Monday", "Tuesday", "Wednesday" };
+        
+            //for (int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i]);
+            //}
+
+            //string fairyTale = "Once upon a time";
+            //string[] storyWords = fairyTale.Split();
+            //Array.Reverse(storyWords);
+
+            //for (int i = 0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+
+            //string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+            //foreach (string month in months)
+            //{
+            //    Console.WriteLine(month);
+            //}
+
+            string[] bands = { "Neil Young", "The Kinks", "The Magnetic Fields", "A Tribe Called Quest", "Those Darlins" };
+
+            foreach (string lalalala in bands)
+            {
+                Console.WriteLine(lalalala);
+            }
+
+        }
+    }
+}
